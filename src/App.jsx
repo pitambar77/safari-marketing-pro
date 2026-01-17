d46c2e0ecd2d77e4f28c6e9b950b12fadd22fea7
pitambar77 <pitambermajhi33@gmail.com>
2026-01-17 20:17:09 +0530

    Updated page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-import React from 'react'
-import GoogleAdd from './pages/googleAdd/GoogleAdd'
-import WebDesign from './pages/webDesign/WebDesign'
-import SocialMedia from './pages/socialMedia/SocialMedia'
+import Layout from "./assets/Layout";
 
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import Layout from './assets/Layout'
+import GoogleAdd from "./pages/googleAdd/GoogleAdd";
+import WebDesign from "./pages/webDesign/WebDesign";
+import SocialMedia from "./pages/socialMedia/SocialMedia";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <>
     <Route path="/" element={<Layout />}>
-        <Route index element={<GoogleAdd />} />
-        <Route path='web-designing' element={<WebDesign />} />
-        <Route path='social-media-marketing' element={<SocialMedia />} />
+      <Route index element={<GoogleAdd />} />
+      <Route path="web-designing" element={<WebDesign />} />
+      <Route path="social-media-marketing" element={<SocialMedia />} />
     </Route>
-    </>
   )
-)
-const App = () => (<><RouterProvider router={router}/></>)
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
-
