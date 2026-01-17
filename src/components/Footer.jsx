39c525f40dbf6d4e96819519c8178d0820adc93e
pitambar77 <pitambermajhi33@gmail.com>
2026-01-17 12:26:31 +0530

    new update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,15 @@ import { FaInstagram } from "react-icons/fa6";
 import { IoLogoYoutube } from "react-icons/io";
 import manorama from "../assets/images/footer-manorama-logo.png";
 import star from "../assets/images/footer5-star-rating.png";
+import clutchlogo from "../assets/images/clutch-logo.svg";
+import partner1 from "../assets/images/new-part-1.png";
+import partner2 from "../assets/images/new-part-2.png";
+import partner3 from "../assets/images/new-part-3.png";
+import partner4 from "../assets/images/new-part-4.png";
 
 const Footer = () => {
   return (
+    <div className="smp-footer-parent">
     <div className="smp-footer-sec">
         <div className="container">
             <div className="row smp-footer">
@@ -60,6 +66,7 @@ const Footer = () => {
                         <h4>Reviews From One Of Most Trusted Partners</h4>
                         <div className="footer-review-box">
                             <div className="inner-foo-review">
+                                <div className="footer-clut-bord">
                                 <div className="footer-headiew">
                                     <img src={manorama} alt="" />
                                     <Link to="#">Manorama Web Solutions Private Limited Reviews</Link>
@@ -71,6 +78,39 @@ const Footer = () => {
                                         <Link to="#">26 reviews</Link>
                                     </div>
                                 </div>
+                                <div className="footer-rating-point">
+                                    <div className="footer-rat-point">                                      
+                                        <div class="widget_rating__info">
+                                            <a href="#">
+                                            <p class="rating__info-name">Scheduling</p>
+                                            <p class="rating__info-rating">4.9</p>
+                                             </a>
+                                        </div>
+                                        <div class="widget_rating__info">
+                                            <a href="#">
+                                            <p class="rating__info-name">Quality</p>
+                                            <p class="rating__info-rating">4.9</p>
+                                             </a>
+                                        </div>
+                                        <div class="widget_rating__info">
+                                            <a href="#">
+                                            <p class="rating__info-name">Cost</p>
+                                            <p class="rating__info-rating">4.8</p>
+                                             </a>
+                                        </div>
+                                        <div class="widget_rating__info">
+                                            <a href="#">
+                                            <p class="rating__info-name">Would Refer</p>
+                                            <p class="rating__info-rating">4.9</p>
+                                             </a>
+                                        </div>
+                                                                     
+                                    </div>
+                                </div>
+                                 <div className="clutch-btn-box">
+                                            <a href="">Powered by <img src={clutchlogo} alt="" /></a>
+                                        </div> 
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -78,6 +118,31 @@ const Footer = () => {
 
             </div>
         </div>
+    </div>
+    <div className="smp-footer-copwrite">
+        <div className="container">
+            <div className="row smp-footer-cop">
+                <div className="col-md-5">
+                    <div className="smp-footer-cop-box">
+                        <ul>
+                            <li>© 2026 Safari Marketing Pro</li>
+                            <li><a href="#">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-md-7">
+                    <div className="smp-footer-partner">
+                        <ul>
+                            <li><img src={partner1} alt="" className="w-100"/></li>
+                            <li><img src={partner2} alt="" className="w-100" /></li>
+                            <li><img src={partner3} alt="" className="w-100" /></li>
+                            <li><img src={partner4} alt="" className="w-100" /></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
   )
 }
